9902cf3546c6a0bd601f4e8b4e7eb0191049d669
Andrew Menard <apmenard1993@gmail.com>
2019-08-14 23:36:22 -0600

    Add initial game screen
    - add flask bootstrap
    
    - create index.html with WIP setup for board
    
    - create new endpoint for submitting a move to the server
    
    - add requirements.txt

--- server/index.py
+++ server/index.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 
 from game import Display
+from game.Board import Board
 
 app = Flask(__name__)
+Bootstrap(app)
+board = Board()
+board.reset_board(board.boardArray)
+
+#todo: make submit_move
+def update():
+    moves = board.get_valid_moves('X')
+    board.boardArray = board.make_move('X', moves[0][0], moves[0][1]).boardArray
 
 
 @app.route('/')
 def index():
-    return Display.get_intro()
+    return render_template('index.html', gameArray=board.boardArray)
+
+
+@app.route('/submitMove', methods=['POST'])
+def submit_move():
+    update()
+    return 'blah'
 
 
 if __name__ == '__main__':
