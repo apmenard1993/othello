f36ec480f87924d9418b42d4cfe474d139ff1285
Andrew Menard <apmenard1993@gmail.com>
2019-08-17 17:37:44 -0600

    Add a bunch of setup files for heroku to be happy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='othello',
+    version='0.0.1',
+    packages=['game', 'server'],
+    url='',
+    license='',
+    author='apmenard1993',
+    author_email='',
+    description='An othello game and web interface.'
+)
